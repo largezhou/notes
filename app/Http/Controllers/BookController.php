6ae82dce650a099c6baae34de30dc929a78f636a
largezhou <largezhou@163.com>
2018-10-23 09:37:40 +0800

    完成书籍的添加

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\BookRequest;
 use App\Http\Resources\BookResource;
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 
 class BookController extends Controller
 {
@@ -18,6 +19,54 @@ class BookController extends Controller
 
     public function store(BookRequest $request)
     {
-        $book = Book::addBook($request->all());
+        $files = $this->handleUploadFile($request);
+        $data = $request->all();
+        $data = array_merge($data, $files);
+
+        $book = Book::addBook($data);
+
+        return $this->created(['id' => $book->id]);
+    }
+
+    /**
+     * 返回 201已创建 响应
+     *
+     * @param null $data
+     *
+     * @return \Symfony\Component\HttpFoundation\Response;
+     */
+    protected function created($data = null)
+    {
+        if (is_null($data)) {
+            return response(null, 201);
+        } else {
+            return response()->json($data)->setStatusCode(201);
+        }
+    }
+
+    /**
+     * 保存文件到本地，并返回 键 => 保存路径 的键值对
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    protected function handleUploadFile(Request $request)
+    {
+        $files = $request->file();
+        $driver = \Storage::drive('public');
+
+        $files = array_map(function (UploadedFile $file) use ($driver) {
+            $md5 = md5_file($file);
+            $ext = $file->getClientOriginalExtension();
+
+            $filename = $md5 . ($ext ? ".{$ext}" : '');
+
+            $path = $driver->putFileAs('uploads', $file, $filename);
+
+            return $driver->url($path);
+        }, $files);
+
+        return $files;
     }
 }
