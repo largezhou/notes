60ac0aebc0b4d236e7d5d570db7829b074f9df2c
largezhou <largezhou@163.com>
2018-11-15 22:17:43 +0800

    标签列表按标签热度排序

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -12,7 +12,7 @@ class TagController extends Controller
 {
     public function index(TagFilter $tagFilter)
     {
-        $tags = Tag::withCount('targets')->filter($tagFilter)->get();
+        $tags = Tag::withCount('targets')->orderBy('targets_count', 'desc')->filter($tagFilter)->get();
 
         return TagResource::collection($tags);
     }
