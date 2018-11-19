19790aa3f7c40831e6780df4444fb0e621ae1481
largezhou <largezhou@163.com>
2018-11-19 17:11:57 +0800

    把BookActions改为RequestActions

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -12,23 +12,23 @@ use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\Traits\BookActions;
+use Tests\Traits\RequestActions;
 
 class BookTest extends TestCase
 {
     use DatabaseMigrations;
-    use BookActions;
+    use RequestActions;
 
     public function testPostCreateBook()
     {
         // 未登录
-        $this->postCreateBook()
+        $this->postCreateResource('books')
             ->assertStatus(401);
 
         Model::clearBootedModels();
         $this->login();
 
-        $this->seeErrorText($this->postCreateBook())
+        $this->seeErrorText($this->postCreateResource('books'))
             ->assertSee('title')
             ->assertSee('total')
             ->assertSee('cover');
@@ -42,7 +42,7 @@ class BookTest extends TestCase
         // 不填已读
         $input = $book;
         unset($input['read']);
-        $res = $this->postCreateBook($input);
+        $res = $this->postCreateResource('books', $input);
         $res->assertStatus(201);
         $this->assertDatabaseHas('books', [
             'id'   => 1,
@@ -52,26 +52,26 @@ class BookTest extends TestCase
         // 已读大于总页数
         $input = $book;
         $input['read'] = $input['total'] + 1;
-        $this->seeErrorText($this->postCreateBook($input), '不能大于' . $input['total']);
+        $this->seeErrorText($this->postCreateResource('books', $input), '不能大于' . $input['total']);
 
         // 日期格式不对
         $input = $book;
         $input['started_at'] = 'not a date';
-        $this->seeErrorText($this->postCreateBook($input), '格式不对呀');
+        $this->seeErrorText($this->postCreateResource('books', $input), '格式不对呀');
 
         // 封面不是图片文件
         $input = $book;
         $input['cover'] = 'not a file';
-        $this->seeErrorText($this->postCreateBook($input), '不是图片不行的');
+        $this->seeErrorText($this->postCreateResource('books', $input), '不是图片不行的');
 
         // 是否隐藏数据是否是 boolean 值
         $input = $book;
         $input['hidden'] = 'not a boolean';
-        $this->seeErrorText($this->postCreateBook($input), '只能是是还是不是');
+        $this->seeErrorText($this->postCreateResource('books', $input), '只能是是还是不是');
 
         // 最终创建成功，即使填了 deleted_at 也不会设置
         $input = $book;
-        $res = $this->postCreateBook($input);
+        $res = $this->postCreateResource('books', $input);
         $res->assertStatus(201)
             ->assertSee('id');
 
@@ -86,20 +86,20 @@ class BookTest extends TestCase
         $this->prepareBooks();
 
         // 未登录的情况，软删除和隐藏的看不到，即使开启了编辑模式
-        $this->getBooks(['edit_mode' => 1])
+        $this->getResources('books', ['edit_mode' => 1])
             ->assertStatus(200)
             ->assertJsonCount(8);
 
         $this->login();
         Model::clearBootedModels();
 
-        $this->getBooks()
+        $this->getResources('books')
             ->assertStatus(200)
             ->assertJsonCount(9)
             ->assertSee('hidden')
             ->assertSee('deleted_at');
 
-        $this->getBooks([], true)
+        $this->getResources('books', [], true)
             ->assertJsonCount(10);
     }
 
@@ -108,26 +108,26 @@ class BookTest extends TestCase
         $this->prepareBooks();
 
         // 软删除和隐藏的 404
-        $this->destroyBook(1, true)->assertStatus(404);
-        $this->destroyBook(2, true)->assertStatus(404);
+        $this->destroyResource('books', 1, true)->assertStatus(404);
+        $this->destroyResource('books', 2, true)->assertStatus(404);
 
         // 正常的，401
-        $this->destroyBook(3, true)->assertStatus(401);
+        $this->destroyResource('books', 3, true)->assertStatus(401);
 
         $this->login();
         Model::clearBootedModels();
 
         // 已经软删除的在非编辑模式下无法查询到
-        $this->destroyBook(1)->assertStatus(404);
+        $this->destroyResource('books', 1)->assertStatus(404);
         // 编辑模式查询到也只是软删除
-        $this->destroyBook(1, true)->assertStatus(204);
+        $this->destroyResource('books', 1, true)->assertStatus(204);
         $this->assertDatabaseHas('books', [
             'id'         => 1,
             'deleted_at' => Book::find(1)->deleted_at,
         ]);
 
         // 正常软删除
-        $this->destroyBook(2)
+        $this->destroyResource('books', 2)
             ->assertStatus(204);
         $this->assertDatabaseHas('books', [
             'id'         => 2,
@@ -139,9 +139,9 @@ class BookTest extends TestCase
     {
         $this->prepareBooks();
 
-        $this->forceDestroyBook(1, true)->assertStatus(404);
-        $this->forceDestroyBook(2, true)->assertStatus(404);
-        $this->forceDestroyBook(3, true)->assertStatus(404);
+        $this->forceDestroyResource('books', 1, true)->assertStatus(404);
+        $this->forceDestroyResource('books', 2, true)->assertStatus(404);
+        $this->forceDestroyResource('books', 3, true)->assertStatus(404);
 
 
         Model::clearBootedModels();
@@ -151,11 +151,11 @@ class BookTest extends TestCase
         Note::find(1)->tags()->save(make(Tag::class));
 
         // 没有被软删除的不能彻底删除
-        $this->forceDestroyBook(2)->assertStatus(404);
-        $this->forceDestroyBook(3)->assertStatus(404);
+        $this->forceDestroyResource('books', 2)->assertStatus(404);
+        $this->forceDestroyResource('books', 3)->assertStatus(404);
 
         // 已经软删除的，可以彻底删除
-        $this->forceDestroyBook(1)
+        $this->forceDestroyResource('books', 1)
             ->assertStatus(204);
 
         // 删除后，笔记、笔记和标签的关联一并删除
@@ -173,9 +173,9 @@ class BookTest extends TestCase
         $this->prepareBooks();
         $this->prepareNotes();
 
-        $this->getBook(1, [], true)->assertStatus(404);
-        $this->getBook(2, [], true)->assertStatus(404);
-        $this->getBook(3, [], true)
+        $this->getResource('books', 1, [], true)->assertStatus(404);
+        $this->getResource('books', 2, [], true)->assertStatus(404);
+        $this->getResource('books', 3, [], true)
             ->assertStatus(200)
             ->assertDontSee('hidden')
             ->assertDontSee('deleted_at')
@@ -187,18 +187,18 @@ class BookTest extends TestCase
         $this->login();
 
         // 软删除的
-        $this->getBook(1)->assertStatus(404);
-        $this->getBook(1, [], true)->assertStatus(200);
+        $this->getResource('books', 1)->assertStatus(404);
+        $this->getResource('books', 1, [], true)->assertStatus(200);
 
         // 隐藏的
-        $this->getBook(2)
+        $this->getResource('books', 2)
             ->assertJsonFragment(['hidden' => '1'])
             ->assertJsonFragment(['deleted_at' => null])
             ->assertJsonFragment(['notes_count' => 9])
             ->assertJsonCount(9, 'notes');
 
         // 笔记编辑模式显示所有
-        $this->getBook(2, [], true)
+        $this->getResource('books', 2, [], true)
             ->assertJsonFragment(['notes_count' => 10])
             ->assertJsonCount(10, 'notes');
     }
@@ -209,19 +209,19 @@ class BookTest extends TestCase
         $this->prepareNotes();
 
         // 默认笔记所属页数倒序
-        $this->assertOrderBy($this->getBook(4), 'page', 'desc', 'notes');
+        $this->assertOrderBy($this->getResource('books', 4), 'page', 'desc', 'notes');
 
-        $this->assertOrderBy($this->getBook(4, [
+        $this->assertOrderBy($this->getResource('books', 4, [
             '_sort_field' => 'page',
             '_sort_type'  => 'asc',
         ]), 'page', 'asc', 'notes');
 
-        $this->assertOrderBy($this->getBook(4, [
+        $this->assertOrderBy($this->getResource('books', 4, [
             '_sort_field' => 'created_at',
             '_sort_type'  => 'asc',
         ]), 'created_at', 'dsc', 'notes');
 
-        $this->assertOrderBy($this->getBook(4, [
+        $this->assertOrderBy($this->getResource('books', 4, [
             '_sort_field' => 'created_at',
             '_sort_type'  => 'desc',
         ]), 'created_at', 'desc', 'notes');
@@ -231,19 +231,19 @@ class BookTest extends TestCase
     {
         $this->prepareBooks();
 
-        $this->updateBook(1)->assertStatus(404);
-        $this->updateBook(2)->assertStatus(404);
-        $this->updateBook(3)->assertStatus(401);
+        $this->updateResource('books', 1)->assertStatus(404);
+        $this->updateResource('books', 2)->assertStatus(404);
+        $this->updateResource('books', 3)->assertStatus(401);
 
         Model::clearBootedModels();
         $this->login();
 
         // 显示
-        $this->updateBook(2, ['hidden' => false])->assertStatus(200);
+        $this->updateResource('books', 2, ['hidden' => false])->assertStatus(200);
         $this->assertDatabaseHas('books', ['id' => 2, 'hidden' => 0]);
 
         // 隐藏
-        $this->updateBook(2, ['hidden' => true])->assertStatus(200);
+        $this->updateResource('books', 2, ['hidden' => true])->assertStatus(200);
         $this->assertDatabaseHas('books', ['id' => 2, 'hidden' => 1]);
     }
 
@@ -252,9 +252,9 @@ class BookTest extends TestCase
         $this->login();
         $this->prepareBooks();
 
-        $this->updateBook(1, ['deleted_at' => null])->assertStatus(404);
+        $this->updateResource('books', 1, ['deleted_at' => null])->assertStatus(404);
 
-        $this->updateBook(1, ['deleted_at' => null], true)->assertStatus(200);
+        $this->updateResource('books', 1, ['deleted_at' => null], true)->assertStatus(200);
         $this->assertDatabaseHas('books', ['id' => 1, 'deleted_at' => null]);
     }
 
@@ -264,25 +264,25 @@ class BookTest extends TestCase
         $this->prepareBooks();
 
         // 啥都不更新
-        $this->updateBook(3)->assertStatus(200);
+        $this->updateResource('books', 3)->assertStatus(200);
 
         $book = Book::find(3)->toArray();
         $book['cover'] = asset($book['cover']);
         unset($book['updated_at']);
 
         // 啥都不更新
-        $this->updateBook(3, $book)->assertStatus(200);
+        $this->updateResource('books', 3, $book)->assertStatus(200);
 
         $this->assertDatabaseHas('books', Book::find(3)->toArray());
 
         // 更新错误的封面
         $input = $book;
         $input['cover'] = 'not a cover';
-        $this->seeErrorText($this->updateBook(3, $input), '不是图片不行的');
+        $this->seeErrorText($this->updateResource('books', 3, $input), '不是图片不行的');
 
         // 更新正确的封面
         $input['cover'] = UploadedFile::fake()->image('cover.jpg');
-        $this->updateBook(3, $input)->assertStatus(200);
+        $this->updateResource('books', 3, $input)->assertStatus(200);
         $input['cover'] = '/uploads/' . md5_file($input['cover']) . '.jpg';
         $this->assertDatabaseHas('books', $input);
 
@@ -291,7 +291,7 @@ class BookTest extends TestCase
         // 去掉 started_at
         $input = $book;
         $input['started_at'] = null;
-        $this->updateBook(3, $input)->assertStatus(200);
+        $this->updateResource('books', 3, $input)->assertStatus(200);
         $this->assertDatabaseHas('books', ['id' => 3, 'started_at' => null]);
     }
 
@@ -303,33 +303,33 @@ class BookTest extends TestCase
         $book = Book::find(3);
 
         // 只更新 read
-        $this->updateBook(3, ['read' => 'not a numeric'])
+        $this->updateResource('books', 3, ['read' => 'not a numeric'])
             ->assertStatus(422)
             ->assertSee(json_encode(['read' => ['已读要一个整数']]))
             ->assertDontSee('total');
 
-        $this->updateBook(3, ['read' => '10000'])
+        $this->updateResource('books', 3, ['read' => '10000'])
             ->assertStatus(422)
             ->assertSee(json_encode(['read' => ['已读不能大于' . $book->total]]))
             ->assertDontSee('total');
 
         // 只更新 total
-        $this->updateBook(3, ['total' => 'not a numeric'])
+        $this->updateResource('books', 3, ['total' => 'not a numeric'])
             ->assertStatus(422)
             ->assertSee(json_encode(['total' => ['总页数要一个整数']]))
             ->assertDontSee('read');
 
-        $this->updateBook(3, ['total' => '1'])
+        $this->updateResource('books', 3, ['total' => '1'])
             ->assertStatus(422)
             ->assertSee(json_encode(['total' => ['总页数不能小于' . $book->read]]))
             ->assertDontSee('read');
 
         // read 和 total 同时更新
-        $this->updateBook(3, ['read' => 999, 'total' => 1])
+        $this->updateResource('books', 3, ['read' => 999, 'total' => 1])
             ->assertStatus(422)
             ->assertJsonFragment(['read' => ['已读不能大于1']]);
 
-        $this->updateBook(3, ['read' => 50, 'total' => 100])
+        $this->updateResource('books', 3, ['read' => 50, 'total' => 100])
             ->assertStatus(200);
         $this->assertDatabaseHas('books', ['id' => 3, 'read' => 50, 'total' => 100]);
     }
---
+++ tests/Traits/RequestActions.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Tests\Traits;
+
+use Illuminate\Foundation\Testing\TestResponse;
+
+trait RequestActions
+{
+    /**
+     * 请求资源集合
+     *
+     * @param string $name
+     * @param array  $params
+     * @param bool   $editMode
+     *
+     * @return TestResponse
+     */
+    protected function getResources(string $name, array $params = [], bool $editMode = false): TestResponse
+    {
+        return $this->json('get', route("{$name}.index"), $params, ['Edit-Mode' => $editMode]);
+    }
+
+    /**
+     * 请求添加资源
+     *
+     * @param string $name
+     * @param array  $data
+     *
+     * @return TestResponse
+     */
+    protected function postCreateResource(string $name, array $data = []): TestResponse
+    {
+        return $this->json('post', route("{$name}.store"), $data);
+    }
+
+    /**
+     * 请求软删除一个资源
+     *
+     * @param string $name
+     * @param int    $id
+     * @param bool   $editMode
+     *
+     * @return TestResponse
+     */
+    protected function destroyResource(string $name, int $id, bool $editMode = false): TestResponse
+    {
+        return $this->json('delete', route("{$name}.destroy", [str_plural($name, 1) => $id]), [], ['Edit-Mode' => $editMode]);
+    }
+
+    /**
+     * 请求彻底删除一个资源
+     *
+     * @param string $name
+     * @param int    $id
+     * @param bool   $editMode
+     * @param string $deletedName
+     *
+     * @return TestResponse
+     */
+    protected function forceDestroyResource(string $name, int $id, bool $editMode = false, string $deletedName = null): TestResponse
+    {
+        $deletedName = $deletedName ?: 'deleted' . ucfirst(str_plural($name, 1));
+
+        return $this->json('delete', route("{$name}.force_destroy", [$deletedName => $id]), [], ['Edit-Mode' => $editMode]);
+    }
+
+    /**
+     * 请求获取单个资源
+     *
+     * @param string $name
+     * @param int    $id
+     * @param array  $params
+     * @param bool   $editMode
+     *
+     * @return TestResponse
+     */
+    protected function getResource(string $name, int $id, array $params = [], bool $editMode = false): TestResponse
+    {
+        return $this->json('get', route("{$name}.show", [str_plural($name, 1) => $id]), $params, ['Edit-Mode' => $editMode]);
+    }
+
+    /**
+     * 请求更新一个资源
+     *
+     * @param string $name
+     * @param int    $id
+     * @param array  $data
+     * @param bool   $editMode
+     *
+     * @return TestResponse
+     */
+    protected function updateResource(string $name, int $id, array $data = [], bool $editMode = false): TestResponse
+    {
+        return $this->json('put', route("{$name}.update", [str_plural($name, 1) => $id]), $data, ['Edit-Mode' => $editMode]);
+    }
+}
