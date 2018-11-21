bbb4c0ec1165239731c4683724966d5bc5a09bf2
largezhou <largezhou@163.com>
2018-11-21 21:29:26 +0800

    调整seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 use App\Models\Note;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Book;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(BookTableSeeder::class);
+        $this->call(PostTableSeeder::class);
         $this->call(TagTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
 
@@ -23,10 +27,6 @@ class BookTableSeeder extends Seeder
 {
     public function run()
     {
-        Book::truncate();
-        Note::truncate();
-        Tag::truncate();
-
         factory(Book::class, 10)->create()->each(function (Book $book) {
             $notesData = factory(Note::class, 10)->make()->each(function (Note $note) use ($book) {
                 $note->page = mt_rand(1, $book->read);
@@ -37,16 +37,35 @@ class BookTableSeeder extends Seeder
     }
 }
 
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(Post::class, 20)->create();
+    }
+}
+
 class TagTableSeeder extends Seeder
 {
     public function run()
     {
-        Tag::truncate();
-        DB::table('model_tags')->truncate();
-
         $tags = factory(Tag::class, 50)->create()->pluck('id')->toArray();
         Note::all()->each(function (Note $note) use ($tags) {
             $note->tags()->attach(array_random($tags, 5));
         });
+        Post::all()->each(function (Post $post) use ($tags) {
+            $post->tags()->attach(array_random($tags, 5));
+        });
+    }
+}
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(User::class, 1)->create([
+            'username'  => 'largezhou',
+            'password' => bcrypt('000000'),
+        ]);
     }
 }
