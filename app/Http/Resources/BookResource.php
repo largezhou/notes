4e63ea210f5845748f9f2d7f480391f8a73334de
largezhou <largezhou@163.com>
2019-01-24 23:45:34 +0800

    书籍更新时，页数返回的是字符串类型，导致判断出错，进度百分比算错

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -7,16 +7,16 @@ class BookResource extends JsonResource
     public function data($request)
     {
         return [
-            'id'          => $this->id,
-            'title'       => $this->title,
-            'started_at'  => $this->started_at ? date('Y-m-d', strtotime($this->started_at)) : null,
-            'created_at'  => $this->created_at,
-            'updated_at'  => $this->updated_at,
-            'deleted_at'  => $this->deleted_at,
-            'hidden'      => $this->hidden,
-            'cover'       => $this->cover ? asset($this->cover) : '',
-            'read'        => $this->read,
-            'total'       => $this->total,
+            'id' => $this->id,
+            'title' => $this->title,
+            'started_at' => $this->started_at ? date('Y-m-d', strtotime($this->started_at)) : null,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+            'hidden' => $this->hidden,
+            'cover' => $this->cover ? asset($this->cover) : '',
+            'read' => (int) $this->read,
+            'total' => (int) $this->total,
             'notes_count' => $this->when(isset($this->notes_count), $this->notes_count),
         ];
     }
